4089b73eba6197757254b8bc6f0657a18d110043
CBD-4sure <cbdhangar08@gmail.com>
2025-03-10 19:55:28 +0530

    deploy update 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
 /* eslint-disable react/react-in-jsx-scope */
-import logo from "../assets/image.png"
-export default function Header(){
+import logo from "../assets/image.png";
+export default function Header() {
 	return (
 		<div className="header">
-            <div className="logo">
-                <img src={logo} alt="logo" style={{'width': '100%','height':'100%','objectFit':'cover'} }></img>
-            </div>
-            <div className="searchbar">
-                <input type="text" placeholder="search"></input>
-                <button>Search</button>
-            </div>
-            <div className="login">
-                <a>Login</a>
-            </div>
+			<div className="logo">
+				<img
+					src={logo}
+					alt="logo"
+					style={{
+						width: "100%",
+						height: "100%",
+						objectFit: "cover",
+					}}
+				></img>
+			</div>
+			<div className="searchbar">
+				<input type="text" placeholder="search"></input>
+				<button>Search</button>
+			</div>
+			<div className="login">
+				<a>Login</a>
+			</div>
 		</div>
 	);
-};
-
+}
