34f8afa05ad66e5da75621ae4f436b36ef22b633
CBD-4sure <cbdhangar08@gmail.com>
2025-03-11 21:56:28 +0530

    added shimmer

---
+++ src/components/Shimmer.js
@@ -0,0 +1,14 @@
+export default function Shimmer() {
+	return (
+		<div className="shimdiv">
+			<div className="shimmer"></div>
+			<div className="shimmer"></div>
+			<div className="shimmer"></div>
+			<div className="shimmer"></div>
+			<div className="shimmer"></div>
+			<div className="shimmer"></div>
+			<div className="shimmer"></div>
+			<div className="shimmer"></div>
+		</div>
+	);
+}
