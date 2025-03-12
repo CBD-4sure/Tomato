a34bb41221eb40a35715038c92e9538403c87af3
CBD-4sure <cbdhangar08@gmail.com>
2025-03-12 11:41:21 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./About";
+
+const appRouter = createBrowserRouter([
+	{ path: "/", element: <App /> },
+	{ path: "/about", element: <About /> },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={appRouter} />
 	</StrictMode>
 );
